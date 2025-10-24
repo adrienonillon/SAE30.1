e7c3243f7011706aa961bb7d423587a8c5cbcb79
adrienonillon <adrien.onillon@etu.unilim.fr>
2025-10-24 09:28:21 +0200

    Manque plus que les liens

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,26 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
+
 export default defineConfig({
-  plugins: [tailwindcss()],
-  base: "evaluation_tailwind",
+ plugins: [tailwindcss()],
+ base: "/repo/",
+ build: {
+   rollupOptions: {
+     input: {
+       main: resolve(__dirname, "index.html"),
+       detail: resolve(__dirname, "page-detail.html"),
+       panier: resolve(__dirname, "page-panier.html"),
+       panier_vide: resolve(__dirname, "page-panier_vide.html"),
+       information_commande: resolve(__dirname, "page-information_commande.html"),
+       validation_historique: resolve(__dirname, "validation_historique.html"),
+       historique_commande: resolve(__dirname, "historique_commande.html"),
+       inscription: resolve(__dirname, "inscrition.html"),
+       connexion: resolve(__dirname, "page-connexion.html"),
+       invité_connexion: resolve(__dirname, "page-invité_connexion.html"),
+       // ajoutez d'autres pages si nécessaire
+     },
+   },
+ },
 });
